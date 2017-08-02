98e410c0db69249e390f521969df4373e66480d2
Kendall <adkins.kendall90@gmail.com>
2017-08-02 18:37:27 -0400

    computer crash panic commit

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -4,4 +4,22 @@ class CharactersController < ApplicationController
     @character = Character.find(params[:id])
   end
 
+  def edit
+    @house = House.find(params[:id])
+    @character = Character.find(params[:id])
+  end
+
+  def update
+    @house = House.find(paramas[:id])
+    @character = Character.find(params[:id])
+    @character.update!(character_params)
+
+    redirect_to house_character_path(@house, @character)
+  end
+
+private
+  def character_params
+    params.require(:character).permit(:name, :title, :quote, :img_url)
+  end
+
 end
