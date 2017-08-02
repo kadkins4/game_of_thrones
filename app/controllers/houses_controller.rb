69c1a8618e26c1023f8ecc7f56a9e2b728309145
Kendall <adkins.kendall90@gmail.com>
2017-08-02 16:39:38 -0400

    house

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -4,4 +4,22 @@ class HousesController < ApplicationController
     @houses = House.all
   end
 
+  def show
+    @house = House.find(params[:id])
+  end
+
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to house_path(@house)
+  end
+
+private
+  def house_params
+    params.require(:house).permit(:name, :sigil, :motto, :location, :banner_img)
+  end
+
 end
