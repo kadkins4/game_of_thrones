ffd7f0968ddef0ad07dafa54aec311508e96ba6b
Kendall <adkins.kendall90@gmail.com>
2017-08-02 19:20:35 -0400

    characters finish

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,14 +5,37 @@ class CharactersController < ApplicationController
   end
 
   def edit
-    @house = House.find(params[:id])
+    @house = House.find(params[:house_id])
     @character = Character.find(params[:id])
   end
 
   def update
-    @house = House.find(paramas[:id])
+    @house = House.find(params[:house_id])
     @character = Character.find(params[:id])
     @character.update!(character_params)
+    flash[:notice] = "#{@character.name} has been updated!"
+
+    redirect_to house_character_path(@house, @character)
+  end
+
+  def new
+    @house = House.find(params[:house_id])
+    @character = Character.new
+  end
+
+  def create
+    @house = House.find(params[:house_id])
+    @character = @house.characters.create(character_params)
+    flash[:notice] = "#{@character.name} has been created!"
+
+    redirect_to house_path(@house)
+  end
+
+  def destroy
+    @house = House.find(params[:house_id])
+    @character = Character.find(params[:id])
+    @character.destroy!
+    flash[:notice] = "#{@character.name} has perished!...typical"
 
     redirect_to house_character_path(@house, @character)
   end
